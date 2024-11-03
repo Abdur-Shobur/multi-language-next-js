60a5e5a57a2220d277548d8d9498c78f46acf0f3
abdur-shobur <abdurshobur.developer@gmail.com>
2024-11-03 11:10:01 +0600

    update env

--- src/components/action/api-get.ts
+++ src/components/action/api-get.ts
@@ -1,5 +1,7 @@
+import { env } from '@/env';
+
 export async function getData(lang: string) {
-	const res = await fetch(`http://localhost:3001/api/language?lang=${lang}`);
+	const res = await fetch(env.BASE_URL + `/api/language?lang=${lang}`);
 	const data = await res.json();
 	return data;
 }
